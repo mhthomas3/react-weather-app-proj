febd103436b6955745351592d0ffe7843f86f4ed
mhthomas3 <morganhthomas93@gmail.com>
2021-06-07 22:50:13 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     hello
+      <div className="container">
+        <btn className="btn btn-primary">hello</btn>
+      </div>
     </div>
   );
 }
