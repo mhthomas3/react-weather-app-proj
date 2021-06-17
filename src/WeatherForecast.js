6a4b2ded51aa224c00efbc6a7b64b556d19b2b25
mhthomas3 <morganhthomas93@gmail.com>
2021-06-16 21:10:45 -0500

    added forecast update functionality

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import {React , useState} from 'react';
+import {React , useState, useEffect} from 'react';
 import WeatherForecastDay from './WeatherForecastDay';
 import './WeatherForecast.css';
 import axios from 'axios';
@@ -6,6 +6,10 @@ import axios from 'axios';
 export default function WeatherForecast(props){
     let [loaded, setLoaded] =useState(false);
     let [forecast, setForecast] = useState(null);
+    
+    useEffect(()=>{
+        setLoaded(false);
+    }, [props.coordinates]);
 
     function handleResponse(response){
         setForecast(response.data.daily)
@@ -33,7 +37,7 @@ export default function WeatherForecast(props){
         let latitude = props.coordinates.lat;
         let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`
         axios.get(apiUrl).then(handleResponse)
-        return "hi";
+        return null;
         
     }
 }
